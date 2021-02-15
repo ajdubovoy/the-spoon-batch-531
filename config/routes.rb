ab0c0bb3cc4de776a6a68e68814cc372525ba9c9
Alexander Dubovoy <ajdubovoy@gmail.com>
2021-02-15 11:02:34 +0100

    livecode

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+ 
+  root to: "pages#home"
+
+  get "about", to: "pages#about", as: :about
+# VERB  PATH        CONTROLLER#ACTION  PREFIX
+
+  get "contact", to: "pages#contact", as: :contact
+
+  # get "our_stuff", to: "pages#our_stuff"
 end
